ec4494c30500bd37550d61c8bf80e8983ddb1ab8
s-sakuda <sakuch03@gmail.com>
2021-10-12 23:58:33 +0900

    整形

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
-	<div id="app">
-		<img alt="Vue logo" src="./assets/logo.png" />
-		<HelloWorld msg="Welcome to Your Vue.js App" />
-		<button @click="open()">モーダルを表示する</button>
-		<Modal v-if="isOpen" />
-	</div>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <button @click="open()">モーダルを表示する</button>
+    <Modal v-if="isOpen" />
+  </div>
 </template>
 
 <script>
@@ -12,42 +12,42 @@ import HelloWorld from './components/HelloWorld.vue'
 import Modal from './components/Modal.vue'
 
 export default {
-	name: 'App',
-	components: {
-		HelloWorld,
-		Modal,
-	},
-	computed: {
-		isOpen() {
-			return this.$store.state.modal.isOpen
-		},
-	},
-	methods: {
-		open() {
-			const payload = {
-				isOpen: true,
-				message: '変更しますがよろしいですか？',
-			}
-			this.$store
-				.dispatch('actionModalOpen', payload)
-				.then(() => {
-					// 「OK」ボタンが押された時の処理をここに書く
-				})
-				.catch(() => {
-					// 「キャンセル」ボタンが押された時の処理をここに書く
-				})
-		},
-	},
+  name: 'App',
+  components: {
+    HelloWorld,
+    Modal,
+  },
+  computed: {
+    isOpen() {
+      return this.$store.state.modal.isOpen
+    },
+  },
+  methods: {
+    open() {
+      const payload = {
+        isOpen: true,
+        message: '変更しますがよろしいですか？',
+      }
+      this.$store
+        .dispatch('actionModalOpen', payload)
+        .then(() => {
+          // 「OK」ボタンが押された時の処理をここに書く
+        })
+        .catch(() => {
+          // 「キャンセル」ボタンが押された時の処理をここに書く
+        })
+    },
+  },
 }
 </script>
 
 <style>
 #app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
